9f60c824e1d38d6f3277e616f4a8d6723ac22e13
Kobblander <kobblander@gmail.com>
2014-03-09 14:28:47 +0000

    Initial commit with code Bæring supplied us with under Annað Efni

---
+++ src/app.js
@@ -0,0 +1,14 @@
+var app = angular.module("EvaluationApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+	$routeProvider.when("/", {
+		templateUrl: "templates/home.html",
+		controller: "HomeController"
+	}).when("/evaluation/:evaluationID", {
+		templateUrl: "templates/evaluation.html",
+		controller: "EvaluationController"
+	}).when("/evaluation/", {
+		templateUrl: "templates/evaluation.html",
+		controller: "EvaluationController"
+	}).otherwise({ redirectTo: "/"});
+});
---
+++ src/controllers/home.js
@@ -0,0 +1,13 @@
+app.controller("HomeController", [
+	"$scope", "ApiFactory",
+	function($scope, ApiFactory) {
+		ApiFactory.getAllEvaluations().then(function(data) {
+			console.log("Success, data: ", data);
+			$scope.evaluations = data;
+		}, function(errorMessage) {
+			console.log("Error: " + errorMessage);
+		}, function(updateMessage) {
+			console.log("Update: " + updateMessage);
+		});
+	}
+]);
