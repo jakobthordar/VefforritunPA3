6cd5185cb361fb07d2f05aec6c9044a39ea291cf
Kobblander <kobblander@gmail.com>
2014-03-18 18:18:58 +0000

    Added a directive and some css yay 10%

--- src/js/directives/allEvalsDctv.js
+++ src/js/directives/allEvalsDctv.js
@@ -2,7 +2,7 @@ app.directive('myEvals', function() {
     return {
         restrict: 'E',
         controller: 'HomeController',
-        templateUrl: 'templates/partials/myEvalsPartial.html'
+        templateUrl: 'templates/partials/myEvalsPartial.html',
         replace: true,
         link: function(scope, element, attr, homeCtrl){
         },
