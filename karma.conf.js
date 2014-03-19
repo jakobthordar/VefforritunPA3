2f8ba5151403db291868a742d322fd1c09a22acd
Kobblander <kobblander@gmail.com>
2014-03-19 14:53:22 +0000

    Bug fixes on configs and index.html

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angles/angles.js',
-      'bower_components/chartjs/Chart.js',
+      'bower_components/chartjs/Chart.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       'templates/partials/*.html',
       'src/js/**/*.js',
